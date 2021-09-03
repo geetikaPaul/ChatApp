742f7b3b3717473ae89f7eef56affc7cb8e08073
Geetika Paul <geetika2604.95@gmail.com>
2021-09-03 11:25:17 +0530

    one to one chat

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ app.get('/', (req, res) => {
   });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('join', function (data) {    
+      socket.join(data.username);
+      console.log(data.username + ' joined');
+    });
+
+    socket.on('chat message', (data) => {
+        console.log('message: ' + data.msg);
+        //io.emit('chat message', msg);
+        io.to(data.to).emit('chat message', data.from + ' : '+ data.msg);
+        io.to(data.from).emit('chat message', data.from + ' : ' +data.msg);
       });
   });
 
