bd3e59597c0ce475fc592a6f4cc4d3fc8dca0152
Geetika Paul <geetika2604.95@gmail.com>
2021-09-22 16:50:21 +0530

    managing inactive user messages using Cassandra

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const io = new Server(server);
 
 var cassandra = require('cassandra-driver')
 var client = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1' })
+var activeUsers = new Set()
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -16,26 +17,36 @@ io.on('connection', (socket) => {
     socket.on('join', function (data) {    
       socket.join(data.username);
       console.log(data.username + ' has joined');
+      activeUsers.add(data.username)
 
-    var userId = 0
-    client.execute("select * from chatApp.User where name = '" + data.username+"'", function (err, result) {
-      if (err) throw err
-      //userId = result.rows[0].userId;
-      console.log(result.rows[0].userId);
-    })
+    client.execute("select * from chatApp.messages where receiver = {username : '" + data.username+"', mobile : '123456'}", function (err, result) {
+        if (err) throw err
+        result.rows.forEach(x => {
+            x.messages.forEach(m => {
+              console.log(m.txt);
+              io.to(data.username).emit('chat message', x.sender.username + ' : '+ m.txt);
+            });
+        });
+      });
 
-    console.log('user id '+userId);
-    client.execute("select * from chatApp.message where status = 'not sent' and receiver = " + userId, function (err, result) {
-      if (err) throw err
-      console.log(result.rows[0])
-    })
-
+      client.execute("delete from chatApp.messages where receiver = {username : '" + data.username+"', mobile : '123456'}", function (err, result) {
+        if (err) throw err
+        console.log('successfully cleaned up')
+      });
     });
 
     socket.on('chat message', (data) => {
         console.log('message: ' + data.msg);
         //io.emit('chat message', msg);
-        io.to(data.to).emit('chat message', data.from + ' : '+ data.msg);
+        if(activeUsers.has(data.to))
+          io.to(data.to).emit('chat message', data.from + ' : '+ data.msg);
+        else
+        {
+          client.execute("insert into chatApp.messages (receiver, sender, messages) values ({username:'"+data.to+"', mobile:'123456'},{username:'"+data.from+"',mobile:'789884'},[{txt:'"+data.msg+"', time:'2015-04-19'}]);", function (err, result) {
+            if (err) throw err
+            console.log('successfully logged')
+          });
+        }
         io.to(data.from).emit('chat message', data.from + ' : ' +data.msg);
       });
   });
